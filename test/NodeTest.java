d7920be6f02efc051e43ddf6221109fba858d713
Sebastian Regucki <sebreg11@gmail.com>
2022-05-05 23:43:07 +0200

    Node tests

---
+++ test/NodeTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class NodeTest {
+    @Test
+    @DisplayName("Ensure that new node was added to the left edge of given node")
+    public void insertNodeLeft(){
+        Node node = new Node(5);
+        node.insertLeft(8);
+        Assertions.assertEquals(8, node.left.data);
+    }
+    @Test
+    @DisplayName("Ensure that new node was added to the right edge of given node")
+    public void insertNodeRight(){
+        Node node = new Node(2);
+        node.insertRight(10);
+        Assertions.assertEquals(10, node.right.data);
+    }
+
+}
